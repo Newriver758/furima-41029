86730a1f369a8ce5723619a3c9d176d2ee9686e7
Newriver758 <shinkawa0109@gmail.com>
2024-09-10 20:20:51 +0900

    userモデルへのvalidateの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must include @' }
+  validates :password, presence: true, length: { minimum: 6 },
+                       format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'must include both letters and numbers' }
+  validates :password_confirmation, presence: true
+  validates :password, confirmation: true
 end
