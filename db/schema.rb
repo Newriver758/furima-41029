1b1ff4ed05492ea5a51d9b9a0b58412f3f098e96
Newriver758 <shinkawa0109@gmail.com>
2024-09-12 19:24:51 +0900

    Userテーブルにカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_10_101413) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_12_101008) do
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_10_101413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "first_name_kana"
+    t.string "last_name_kana"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
