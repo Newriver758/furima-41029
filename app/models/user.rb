a5153995137e58a641c495b0e699f8fab825956d
Newriver758 <shinkawa0109@gmail.com>
2024-09-14 08:22:08 +0900

    トップページの表示2及びログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,4 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must include @' }
   validates :password, presence: true, length: { minimum: 6 },
                        format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'must include both letters and numbers' }
-  validates :password_confirmation, presence: true
-  validates :password, confirmation: true
 end
