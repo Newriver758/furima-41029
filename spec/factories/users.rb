cd59e771ea6ad065733df711c10e654244e6303c
Newriver758 <shinkawa0109@gmail.com>
2024-09-15 17:20:29 +0900

    テストカラムの修正及び追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false) + '1a' }
     password_confirmation { password }
+    last_name             { Faker::Name.last_name }
+    first_name            { Faker::Name.first_name }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birth_date            { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
