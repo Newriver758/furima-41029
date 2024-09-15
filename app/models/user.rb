c6e52e649975f3a0420a4d236dd69fc0f441f49e
Newriver758 <shinkawa0109@gmail.com>
2024-09-15 17:02:36 +0900

    お名前, 生年月日のバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :nickname, presence: true
+  validates :last_name, presence: true,
+                        format: { with: /\A[一-龥ぁ-んァ-ヶ]+\z/, message: 'must be in full-width characters (kanji, hiragana, katakana)' }
+  validates :first_name, presence: true,
+                         format: { with: /\A[一-龥ぁ-んァ-ヶ]+\z/, message: 'must be in full-width characters (kanji, hiragana, katakana)' }
+  validates :last_name_kana, presence: true,
+                             format: { with: /\A[ァ-ヶー－]+\z/, message: 'must be in full-width katakana' }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[ァ-ヶー－]+\z/, message: 'must be in full-width katakana' }
+  validates :birth_date, presence: true
 end
