5d3367d7beceade27542c99819d93bcea7d70727
Newriver758 <shinkawa0109@gmail.com>
2024-09-15 16:52:58 +0900

    コメントアウトの削除, email, passwordバリデーションの削除, コントローラー編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must include @' }
-  validates :password, presence: true, length: { minimum: 6 },
-                       format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'must include both letters and numbers' }
 end
