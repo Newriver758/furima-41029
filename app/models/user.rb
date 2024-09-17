e411075aa326713908002401534146ac227c4e9a
Newriver758 <shinkawa0109@gmail.com>
2024-09-17 19:53:38 +0900

    「々」, 「ヴァ」の対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :last_name, presence: true,
-                        format: { with: /\A[一-龥ぁ-んァ-ヶー]+\z/, message: 'must be in full-width characters (kanji, hiragana, katakana)' }
+                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be in full-width characters (kanji, hiragana, katakana)' }
   validates :first_name, presence: true,
-                         format: { with: /\A[一-龥ぁ-んァ-ヶー]+\z/, message: 'must be in full-width characters (kanji, hiragana, katakana)' }
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be in full-width characters (kanji, hiragana, katakana)' }
   validates :last_name_kana, presence: true,
                              format: { with: /\A[ァ-ヶー－]+\z/, message: 'must be in full-width katakana' }
   validates :first_name_kana, presence: true,
