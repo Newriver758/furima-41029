042f2f81032dc55da4e753b1302409b72437bb46
Newriver758 <shinkawa0109@gmail.com>
2024-09-19 20:19:34 +0900

    Itemsテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_12_101008) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_19_111447) do
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "category_id"
+    t.integer "condition_id"
+    t.integer "shipping_charge_id"
+    t.integer "prefecture_id"
+    t.integer "shipping_day_id"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -34,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_12_101008) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
