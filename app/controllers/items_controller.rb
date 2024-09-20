b932f74676cef45db59971817ddab463221a6d3e
Newriver758 <shinkawa0109@gmail.com>
2024-09-20 18:29:57 +0900

    Itemsコントローラー, モデル, ビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,23 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+
+    def new
+      @item = Item.new
+    end
+  
+    def create
+      @item = Item.new(item_params)
+      if @item.save
+        redirect_to @item, notice: '商品が正常に出品されました。'
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def item_params
+      params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, :description, :price, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id,
+            presence: true
+  validates :image, presence: true
 end
