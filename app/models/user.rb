5038cc683be04c2383fe4bdab58a2f7d81b09cd1
Newriver758 <shinkawa0109@gmail.com>
2024-09-22 17:47:35 +0900

    validate修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true,
                               format: { with: /\A[ァ-ヶー－]+\z/, message: 'must be in full-width katakana' }
   validates :birthday, presence: true
-  validates :password_complexity
+  validate :password_complexity
 
   private
 
