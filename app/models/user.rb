49dd15d56c5e4d00e76b2488ea403b196f69694b
Newriver758 <shinkawa0109@gmail.com>
2024-09-22 17:40:15 +0900

    ルーティング訂正及びUserモデル誤字修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
-
   validates :nickname, presence: true
   validates :last_name, presence: true,
                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be in full-width characters (kanji, hiragana, katakana)' }
@@ -14,7 +12,7 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true,
                               format: { with: /\A[ァ-ヶー－]+\z/, message: 'must be in full-width katakana' }
   validates :birthday, presence: true
-  validate :password_complexity
+  validates :password_complexity
 
   private
 
