c91045e61406dd40cafbaa8d7b209bc06e5b0405
Newriver758 <shinkawa0109@gmail.com>
2024-09-23 18:36:13 +0900

    単体テストコード, FactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,22 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.sentence(word_count: 15) }
+    category_id { rand(1..10) }
+    condition_id { rand(1..5) }
+    shipping_charge_id { rand(1..5) }
+    prefecture_id { rand(1..47) }
+    shipping_day_id { rand(1..5) }
+    price { rand(300..10_000) }
+
+    trait :invalid do
+      name { nil }
+    end
+
+    trait :low_price do
+      price { 299 }
+    end
   end
 end
