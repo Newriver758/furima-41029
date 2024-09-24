27844b8d0a50cf9517adaf0d937cf8759d17afc1
Newriver758 <shinkawa0109@gmail.com>
2024-09-24 19:50:41 +0900

    JavaScriptの更新及び修正

---
+++ app/javascript/item_price.js
@@ -0,0 +1,6 @@
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+});
+});
