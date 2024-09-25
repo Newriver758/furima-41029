3c048c24454de4b79d6ea67a1b765be3202de6b5
Newriver758 <shinkawa0109@gmail.com>
2024-09-25 21:30:18 +0900

    エラーメッセージの表示及び重複表示の解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,9 +11,9 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path, notice: '商品が正常に出品されました。'
+      redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
