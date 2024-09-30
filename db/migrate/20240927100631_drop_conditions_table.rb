e261906c34946710ba2a90adbccba04a0b3dda07
Newriver758 <shinkawa0109@gmail.com>
2024-09-30 20:53:07 +0900

    ActiveHashに関するテーブルの削除

--- db/migrate/20240927100631_drop_conditions_table.rb
+++
@@ -1,5 +0,0 @@
-class DropConditionsTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :conditions
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_29_091027) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "categories", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -67,24 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_29_091027) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
-  end
-
-  create_table "prefectures", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "shipping_charges", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "shipping_days", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
