fbd2a4e972e750188bc8287d2e85e36c9c37ea61
Newriver758 <shinkawa0109@gmail.com>
2024-10-07 19:34:14 +0900

    Order Formオブジェクトの作成

---
+++ app/models/order_form.rb
@@ -0,0 +1,27 @@
+class OrderForm
+  include ActiveModel::Model
+
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
+
+  # バリデーションの定義
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as 123-4567' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Enter only numbers' }
+    validates :token
+  end
+
+  # データを保存するメソッド
+  def save
+    # 購入履歴を保存
+    order = Order.create(user_id:, item_id:)
+
+    # 住所情報を保存
+    Address.create(postal_code:, prefecture_id:, city:, address:, building:,
+                   phone_number:, order_id: order.id)
+  end
+end
