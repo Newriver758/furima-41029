ab51ffc4abbadee2d8527a2ae295e529cb568045
Newriver758 <shinkawa0109@gmail.com>
2024-10-14 19:43:12 +0900

    テストコードの実装

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -7,7 +7,7 @@ class OrderForm
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as 123-4567' }
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
