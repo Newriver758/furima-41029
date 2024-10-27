2bfd1ab36a44f3820041645aad08cc215c22b1b1
Newriver758 <shinkawa0109@gmail.com>
2024-10-27 18:48:45 +0900

    テストコードの修正及び設定の追記

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe OrderForm, type: :model do
   before do
-    @order_form = FactoryBot.build(:order_form)
+    # ユーザーと商品をFactoryBotで作成し、DBに保存
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+
+    # 生成したユーザーと商品のIDを使用してOrderFormインスタンスを作成
+    @order_form = FactoryBot.build(:order_form, user_id: user.id, item_id: item.id)
   end
 
   describe '配送先情報の保存' do
